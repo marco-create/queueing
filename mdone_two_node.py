066d21bfbc1fa6991ce8e890861ba39e7ee1243e
marco-create <53913990+marco-create@users.noreply.github.com>
2023-11-27 08:31:28 +0100

    Two nodes

--- mdone_two_node.py
+++ mdone_two_node.py
@@ -2,8 +2,8 @@
 
 """
 import random
+import pandas as pd
 import queueing_tool as qt
-import matplotlib.pyplot as plt
 
 # SERVICE TIME
 def ser(t: float):
@@ -38,13 +38,9 @@ g = qt.adjacency2graph(
 q_classes = { 1: qt.QueueServer, 2: qt.QueueServer }
 q_args = {
     1: {
-        'num_server': 1,
-        'arrival_f': lambda t: t + random.expovariate(1.5),  # fast requests
         'service_f': ser
     },
     2: {
-        'num_server': 1,
-        'arrival_f': lambda t: t,
         'service_f': ser
     },
 }
@@ -53,3 +49,41 @@ qn = qt.QueueNetwork(
     q_classes=q_classes,
     q_args=q_args
 )
+
+# init and accept agents
+qn.initialize(nActive=3)
+qn.start_collecting_data()
+
+# SIMULATE
+ag_slow.queue_action(queue=qn)
+qn.simulate(n=10)
+slow_data = qn.get_agent_data(return_header=True)
+cols = (slow_data[1]).split(',')
+
+# inject the faster lambdas
+ag_fast.queue_action(queue=qn)
+qn.simulate(n=10)
+fast_data = qn.get_agent_data()
+
+# remove items that did not leave the system
+clean_data = [item for k, item in fast_data.items() if len(item) > 1]
+
+df_nodeone= pd.DataFrame(data=[item[0] for item in clean_data if item[0, -1] == 0.0], columns=cols)
+df_nodeone['time_spent'] = [item[0, 2] - item[0, 0] for item in clean_data if item[0, -1] == 0.0]
+df_nodeone['rho (arr/ser)'] = [item[0, 0] / item[0, 1] for item in clean_data if item[0, -1] == 0.0]
+df_nodeone['nodes'] = 'first'
+
+df_nodetwo= pd.DataFrame(data=[item[1] for item in clean_data if item[1, -1] == 1.0], columns=cols)
+df_nodetwo['time_spent'] = [item[1, 2] - item[1, 0] for item in clean_data if item[1, -1] == 1.0]
+df_nodetwo['rho (arr/ser)'] = [item[1, 0] / item[1, 1] for item in clean_data if item[1, -1] == 1.0]
+df_nodetwo['nodes'] = 'second'
+
+df_nodeall= pd.DataFrame(data=[item[2] for item in clean_data if len(item) >2], columns=cols)
+df_nodeall['time_spent'] = [item[1, 2] - item[0, 0] for item in clean_data if len(item) >2]
+df_nodeall['rho (arr/ser)'] = [item[0, 0] / item[1, 1] for item in clean_data if len(item) >2]
+df_nodeall['nodes'] = 'tot'
+
+frames = [df_nodeone, df_nodetwo, df_nodeall]
+concat_frames = pd.concat(frames)
+
+concat_frames.to_excel('mdone_two_node.xlsx')
